e6c92aef237fcc1e6cc3b0a204e918ac15512ffc
Sebastian Mihalache <sebimih@gmail.com>
2025-12-28 17:03:42 +0200

    Setup

---
+++ restaurant-manager/src/main/java/com/unibuc/restaurant_manager/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.unibuc.restaurant_manager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello World!";
+    }
+}
