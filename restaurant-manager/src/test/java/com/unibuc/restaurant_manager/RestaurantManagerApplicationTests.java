d8a9ea561c24b0ebd4566737ba2b522ef83689b6
Sebastian Mihalache <sebimih@gmail.com>
2025-12-28 16:56:30 +0200

    Add spring boot

---
+++ restaurant-manager/src/test/java/com/unibuc/restaurant_manager/RestaurantManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibuc.restaurant_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
